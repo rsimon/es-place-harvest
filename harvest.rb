c7e416f4b070cf7d96b8c6a9a66a855d64269ef2
Rainer Simon <rainer.simon@ait.ac.at>
2017-12-05 23:34:24 +0100

    Minor edits

--- harvest.rb
+++ harvest.rb
@@ -2,21 +2,33 @@ require "json"
 require "net/http"
 require "uri"
 
-uri = URI.parse("http://localhost:9200/peripleo/_search?q=item_type:PLACE&size=200&scroll=1m")
+def fetch_inital
+  uri = URI.parse("http://localhost:9200/peripleo/_search?q=item_type:PLACE&size=200&scroll=1m")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
 
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
+  if response.code == "200"
+    result = JSON.parse(response.body)
+    scroll_id = result["_scroll_id"]
+    result["hits"]["hits"].each do |hit|
+      #puts hit
+      #puts "----------------"
+    end
 
-response = http.request(request)
-
-if response.code == "200"
-  result = JSON.parse(response.body)
-  scroll_id = result["scroll_id"]
-  result["hits"]["hits"].each do |hit|
-    puts hit
-    puts "----------------"
+    return scroll_id
+  else
+    puts "ERROR!!!"
   end
 
-else
-  puts "ERROR!!!"
 end
+
+def fetch_next(id)
+  uri = URI.parse("http://localhost:9200/peripleo/_search/scroll?scroll=1m&scroll_id=#{id}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+end
+
+scroll_id = fetch_inital()
+fetch_next(scroll_id)
