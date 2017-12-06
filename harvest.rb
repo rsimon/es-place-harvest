82645830db0bdc1ab5d3feeb0bf8d025c3d88de6
Rainer Simon <rainer.simon@ait.ac.at>
2017-12-06 18:25:18 +0100

    Basic ES scroll loop

--- harvest.rb
+++ harvest.rb
@@ -2,20 +2,24 @@ require "json"
 require "net/http"
 require "uri"
 
-PAGE_SIZE = 1
+PAGE_SIZE = 200
 
 def parse_response(response)
   if response.code == "200"
     result = JSON.parse(response.body)
     scroll_id = result["_scroll_id"]
     result["hits"]["hits"].each do |hit|
-      # puts hit["title"]
-      #puts "----------------"
+      # TODO title, geometry, identifier, match uris, names
+      # TODO write to file
+      source = hit["_source"]
+      puts source["title"]
     end
 
-    return scroll_id
+    if result["hits"]["hits"].length >= PAGE_SIZE
+      return scroll_id
+    end
   else
-    puts "ERROR!!!"
+    puts "-- ERROR --"
   end
 end
 
@@ -27,15 +31,17 @@ def fetch_inital
 end
 
 def fetch_next(id)
-  uri = URI.parse("http://localhost:9200/peripleo/_search/scroll")
+  uri = URI.parse("http://localhost:9200/_search/scroll")
   http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Get.new(uri.request_uri, 'Content-Type' => 'application/json')
-  request.body = "{ \"scroll\" : \"1m\", \"_scroll_id\" : \"#{id}\" }"
-  response = http.request(request)
-
-  puts response
+  request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+  request.body = "{ \"scroll\" : \"1m\", \"scroll_id\" : \"#{id}\" }"
+  parse_response(http.request(request))
 end
 
 scroll_id = fetch_inital()
-puts scroll_id
-fetch_next(scroll_id)
+
+while not scroll_id.to_s.empty? do
+  scroll_id = fetch_next(scroll_id)
+end
+
+puts 'Done.'
